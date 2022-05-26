9b47d3abbaf91fed53d73867b8e50c7273c21875
Leila Mizrahi <leila.mizrahi@sed.ethz.ch>
2022-05-26 14:40:29 +0200

    some explanations for estimate mc. no config file though. sorry

--- runnable_code/estimate_mc.py
+++ runnable_code/estimate_mc.py
@@ -10,7 +10,24 @@ if __name__ == '__main__':
 		mcs,
 		delta_m=0.1,
 		p_pass=0.05,
-		stop_when_passed=False,
+		stop_when_passed=True,
 		verbose=True,
 		n_samples=1000
 	)
+
+	'''
+	magnitude_sample: array of magnitudes
+	mcs: values of mc you want to test. make sure they are rounded correctly, because 3.19999999 will produce weird results.
+	delta_m: magnitude bin size
+	p_pass: p_value above which the catalog is accepted to be complete
+	stop_when_passed: if True, the remaining mc values will not be tested anymore
+	verbose: if True, stuff will be printed while the code is running
+	n_samples: number of samples that are simulated to obtain the p-value
+	
+	
+	see the paper below for details on the method:
+	
+	Leila Mizrahi, Shyam Nandan, Stefan Wiemer 2021;
+	The Effect of Declustering on the Size Distribution of Mainshocks.
+	Seismological Research Letters; doi: https://doi.org/10.1785/0220200231
+	'''
