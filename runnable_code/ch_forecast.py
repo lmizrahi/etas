ddb481708fd517187ceaf6555d8183e9a3178770
Nicolas Schmid <nicolas@breiten.ch>
2022-07-21 11:39:29 +0200

    feat: merge separate configs for ch_forecast

--- runnable_code/ch_forecast.py
+++ runnable_code/ch_forecast.py
@@ -16,33 +16,27 @@ from etas.inversion import invert_etas_params
 if __name__ == '__main__':
 
     # reads configuration for example ETAS parameter inversion
-    with open('../config/ch_invert_etas_config.json', 'r') as f:
-        inversion_config = json.load(f)
+    with open('../config/ch_forecast_config.json', 'r') as f:
+        forecast_config = json.load(f)
 
-    inversion_config['timewindow_end'] = dt.datetime.now()
+    forecast_config['timewindow_end'] = dt.datetime.now()
 
-    parameters = invert_etas_params(inversion_config)
+    parameters = invert_etas_params(forecast_config)
 
-    # simulate 100000 catalog continuations
-    # read configuration in
-    cont_config = '../config/ch_simulate_catalog_continuation_config.json'
-    with open(cont_config, 'r') as f:
-        simulation_config = json.load(f)
-
-    aux_start = pd.to_datetime(inversion_config['auxiliary_start'])
-    prim_start = pd.to_datetime(inversion_config['timewindow_start'])
+    aux_start = pd.to_datetime(forecast_config['auxiliary_start'])
+    prim_start = pd.to_datetime(forecast_config['timewindow_start'])
 
     # end of training period is start of forecasting period
-    forecast_start_date = pd.to_datetime(inversion_config['timewindow_end'])
+    forecast_start_date = pd.to_datetime(forecast_config['timewindow_end'])
     forecast_end_date = forecast_start_date + \
-        dt.timedelta(days=int(simulation_config['forecast_duration']))
+        dt.timedelta(days=int(forecast_config['forecast_duration']))
 
-    coordinates = read_shape_coords(inversion_config['shape_coords'])
+    coordinates = read_shape_coords(forecast_config['shape_coords'])
     poly = Polygon(coordinates)
 
-    fn_train_catalog = inversion_config['fn_catalog']
-    delta_m = inversion_config['delta_m']
-    m_ref = inversion_config.get('m_ref', inversion_config['mc'])
+    fn_train_catalog = forecast_config['fn_catalog']
+    delta_m = forecast_config['delta_m']
+    m_ref = forecast_config.get('m_ref', forecast_config['mc'])
 
     beta = parameters['beta']
 
@@ -58,7 +52,7 @@ if __name__ == '__main__':
                           parse_dates=['time'],
                           dtype={'url': str, 'alert': str})
 
-    sources = pd.read_csv(simulation_config['fn_src'], index_col=0)
+    sources = pd.read_csv(forecast_config['fn_src'], index_col=0)
 
     # xi_plus_1 is aftershock productivity inflation factor. not used here.
     sources['xi_plus_1'] = 1
@@ -79,15 +73,15 @@ if __name__ == '__main__':
         + ' but I am supposed to simulate above ' + str(m_ref)
 
     # background rates
-    ip = pd.read_csv(simulation_config['fn_ip'], index_col=0)
+    ip = pd.read_csv(forecast_config['fn_ip'], index_col=0)
     ip.query('magnitude>=@m_ref -@delta_m/2', inplace=True)
     ip = gpd.GeoDataFrame(
         ip, geometry=gpd.points_from_xy(ip.latitude, ip.longitude))
     ip = ip[ip.intersects(poly)]
 
     # other constants
-    coppersmith_multiplier = inversion_config['coppersmith_multiplier']
-    earth_radius = inversion_config.get('earth_radius', 6.3781e3)
+    coppersmith_multiplier = forecast_config['coppersmith_multiplier']
+    earth_radius = forecast_config.get('earth_radius', 6.3781e3)
 
     print(f'm ref: {m_ref} min magnitude in training '
           f'catalog: {catalog["magnitude"].min()}')
@@ -126,7 +120,7 @@ if __name__ == '__main__':
 
         print('store catalog..')
         os.makedirs(os.path.dirname(
-            simulation_config['fn_store_simulation']), exist_ok=True)
+            forecast_config['fn_store_simulation']), exist_ok=True)
 
         output_cols = [
             'latitude',
@@ -136,6 +130,6 @@ if __name__ == '__main__':
             'is_background']
 
         continuation[output_cols].sort_values(by='time').to_csv(
-            f'{simulation_config["fn_store_simulation"]+str(simulation_i)}.csv')
+            f'{forecast_config["fn_store_simulation"]+str(simulation_i)}.csv')
 
     print('\nDONE!')
